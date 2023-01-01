72cb977e71e0298aa325ec08367d06ecc127dda9
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-01-01 16:25:42 +0000

    add script.js folder

---
+++ script.js
@@ -0,0 +1,15 @@
+function ingredientsHover(){
+	document.getElementById("ingredients").style.fontSize = "300%"
+}
+			
+function ingredientsNormal(){
+	document.getElementById("ingredients").style.fontSize = "100%"
+}
+			
+function preparationHover(){
+	document.getElementById("preparation").style.fontSize = "300%"
+}
+			
+function preparationNormal(){
+	document.getElementById("preparation").style.fontSize = "100%"
+}
