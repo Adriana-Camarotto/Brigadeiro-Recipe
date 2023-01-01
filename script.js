1d81dada1ee1df8253327714ed4b4888bb82c07e
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-01-01 17:17:35 +0000

    add Hover

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+function cookTimeHover(){
+	document.getElementById("cookTime").style.fontSize = "300%"
+}
+			
+function cookTimeNormal(){
+	document.getElementById("cookTime").style.fontSize = "100%"
+}
+			
 function ingredientsHover(){
 	document.getElementById("ingredients").style.fontSize = "300%"
 }
-			
+
 function ingredientsNormal(){
 	document.getElementById("ingredients").style.fontSize = "100%"
 }
-			
+
 function preparationHover(){
 	document.getElementById("preparation").style.fontSize = "300%"
 }
-			
+
 function preparationNormal(){
 	document.getElementById("preparation").style.fontSize = "100%"
 }
